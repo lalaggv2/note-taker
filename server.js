92762636c507faa650c0100d985917985134e13f
Lala <lalagg@hotmail.com>
2020-06-14 12:46:35 -0400

    changing server to app

--- server.js
+++ server.js
@@ -2,27 +2,27 @@
 const express = require("express");
 const path = require("path");
 
-let server = express();
+let app = express();
 const PORT = process.env.PORT || 3000;
 
-server.use(express.static("./public"));
-server.use(express.urlencoded({ extended: true }));
-server.use(express.json());
+app.use(express.static("./public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 //get request for /api/notes
 const apiController = require("./routes/apiRoutes");
-server.use(apiController);
+app.use(apiController);
 
 const htmlController = require("./routes/htmRoutes");
-server.use(htmlController);
+app.use(htmlController);
 
-server.use(express.urlencoded({ extended: true }));
-server.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-server.use('/notes', apiController);
-server.use("/", htmlController)
+app.use('/notes', apiController);
+app.use("/", htmlController)
 
-server.listen(PORT, () => {
-  console.log("server listening on port ", PORT);
+app.listen(PORT, () => {
+  console.log("app listening on port ", PORT);
 });
 
