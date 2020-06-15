8d91304b4c3575c55a540d1d4af67f7b00836352
Lala <lalagg@hotmail.com>
2020-06-15 18:16:10 -0400

    changed port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const path = require("path");
 
 let app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 app.use(express.static("./public"));
 app.use(express.urlencoded({ extended: true }));
